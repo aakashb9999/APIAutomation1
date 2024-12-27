3bf2fffa0b7b81bab2080be7cb2c6ed10e47a7ab
zeelb396 <zeelb396@gmail.com>
2024-12-27 16:11:16 +0530

    config file

--- src/main/java/com/configManager/configurationManager.java
+++ src/main/java/com/configManager/configurationManager.java
@@ -11,29 +11,30 @@ public class configurationManager {
 	
 	public Properties initProp() {
 		
-		String env = System.getProperty("env");
+		//String env = System.getProperty("env");
 		prop = new Properties();
 		try {
-			if (env == null) {
+			//if (env == null) {
 				System.out.println("no env is given...hence running tests on QA env... ");
 				fis = new FileInputStream("./src/test/resources/config/config.properties");
-			} else {
-				System.out.println("Running tests on env: " + env);
-			}
-			switch (env.toLowerCase().trim()) {
-			case "qa":
-			    fis = new FileInputStream("./src/test/resources/config/qa.config.properties");
-			    break;
-			case "dev":
-				fis = new FileInputStream("./src/test/resources/config/dev.config.properties");
-				break;
-			case "stage":
-				fis = new FileInputStream("./src/test/resources/config/stage.config.properties");
-				break;
-			default:
-				fis= new FileInputStream("./src/test/resources/config/config.properties");
-				break;
-		} } catch (FileNotFoundException e) {
+//			} else {
+//				System.out.println("Running tests on env: " + env);
+//			}
+//			switch (env.toLowerCase().trim()) {
+//			case "qa":
+//			    fis = new FileInputStream("./src/test/resources/config/qa.config.properties");
+//			    break;
+//			case "dev":
+//				fis = new FileInputStream("./src/test/resources/config/dev.config.properties");
+//				break;
+//			case "stage":
+//				fis = new FileInputStream("./src/test/resources/config/stage.config.properties");
+//				break;
+//			default:
+//				fis= new FileInputStream("./src/test/resources/config/config.properties");
+//				break;
+//		}
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
