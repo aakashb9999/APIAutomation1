d57ed6da9fc355043847c74322f2d9b55923d368
zeelb396 <zeelb396@gmail.com>
2024-12-27 14:39:25 +0530

    adding allure reports

--- src/test/java/com/base/BaseTest.java
+++ src/test/java/com/base/BaseTest.java
@@ -3,10 +3,12 @@ package com.base;
 import java.util.Properties;
 
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import com.Clients.RestUtils;
 import com.configManager.configurationManager;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 
 public class BaseTest {
 	protected configurationManager config;
@@ -18,6 +20,7 @@ public class BaseTest {
 	
 	@BeforeTest
 	public void setup() {
+		RestAssured.filters(new AllureRestAssured());
 		config = new configurationManager();
 		pro = config.initProp();
 	}
